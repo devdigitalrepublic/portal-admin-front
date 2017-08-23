6a005766612cfccfe143c3cb2a7131a0c1b65e0c
Thiago Montini <thiagomontini@gmail.com>
2017-08-22 21:22:44 -0300

    WIP: mascaras

--- js/controller.js
+++ js/controller.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
   $('.date_time').mask('00/00/0000 00:00:00');
   $('.cep').mask('00000-000');
   $('.phone').mask('0000-0000');
+  $('.mobile').mask('9999-99999');
   $('.phone_with_ddd').mask('(00) 0000-0000');
   $('.cpf').mask('000.000.000-00', {reverse: true});
   $('.money').mask('000.000.000.000.000,00', {reverse: true});
